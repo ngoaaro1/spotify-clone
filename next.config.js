460c463cb3934aef3417284aebd69d2555209fb8
Aaron Ngo <ngoaaro1@msu.edu>
2023-12-18 18:44:00 -0500

    final

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: [
+            "kjxefybrsnjrcqthpkhh.supabase.co"
+        ]
+    }
+}
+
+module.exports = nextConfig
