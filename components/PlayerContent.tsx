98e763f25c0dd244d4fea1816edfcd0191afd2d5
Aaron Ngo <ngoaaro1@msu.edu>
2024-01-24 11:43:48 -0500

    Changed default volume level to half

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -23,7 +23,7 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
     songUrl
 }) => {
     const player = usePlayer();
-    const [volume, setVolume] = useState(1);
+    const [volume, setVolume] = useState(0.5);
     const [isPlaying, setIsPlaying] = useState(false);
 
     const Icon = isPlaying ? BsPauseFill : BsPlayFill;
@@ -91,7 +91,7 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
 
     const toggleMute = () => {
         if (volume === 0) {
-            setVolume(1);
+            setVolume(0.5);
         } else {
             setVolume(0);
         }
